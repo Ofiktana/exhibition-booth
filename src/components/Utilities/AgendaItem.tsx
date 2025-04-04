bf5c6d9205ca88b45d1073cba319a1c295c41596
Tana Ofik <ofik.tana@gmail.com>
2025-04-04 07:38:08 +0100

    sorted out responsiveness of the learning page with line-clamp

--- src/components/Utilities/AgendaItem.tsx
+++ src/components/Utilities/AgendaItem.tsx
@@ -6,40 +6,43 @@ import '../../index.css'
 function AgendaItem({ fields }:any) {
 
   return (
-
-      <div className="backdrop semi-dark-border">
-        <div className="container-outer">
-          <div className="container-top-block">
-            <img 
-              src={fields.imageURL} 
-              alt={`display image for ${fields.title}`} 
-              width='100%' 
-              height='100%'
-              className='w-full h-full'
-            />
-          </div>
-          <div className="container-activity-block">
+    <div className="backdrop semi-dark-border">
+      <div className="container-outer">
+        <div className="container-top-block">
+          <img
+            src={fields.imageURL}
+            alt={`display image for ${fields.title}`}
+            width="100%"
+            height="100%"
+            className="w-full h-full"
+          />
+        </div>
+        <div className="container-activity-block">
           <div className="activity-title">
-               {fields.title}
-           <p><small className="activity-time">
-             <FaRegClock style={{marginRight: '0.5em'}}/>
-             {fields.startTime} - {fields.endTime}</small>
-           </p>
+            {fields.title}
+            <p>
+              <small className="activity-time">
+                <FaRegClock style={{ marginRight: "0.5em" }} />
+                {fields.startTime} - {fields.endTime}
+              </small>
+            </p>
           </div>
-           <div className="activity-desc ">
-             <p className="activity-summary ">
-               {fields.summary.length > 103 ? fields.summary.slice(0,100) + '...' : fields.summary}
-             </p>    
-           </div>
-           <NavLink to={`${fields.slug}`}>
+          <div className="activity-desc ">
+            <p className="activity-summary line-clamp-1">
+              {fields.summary.length > 103
+                ? fields.summary.slice(0, 100) + "..."
+                : fields.summary}
+            </p>
+          </div>
+          <NavLink to={`${fields.slug}`}>
             <button className="learn-more medium-button button dark semi-rounded">
               Learn More
             </button>
-           </NavLink>
-         </div>
+          </NavLink>
         </div>
       </div>
-  )
+    </div>
+  );
 }
 
 export default AgendaItem
